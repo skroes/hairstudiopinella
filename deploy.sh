9fd58db7d68eeef383d54c1b07ead322d16f8b13
Serge Kroes <serge@kroescontrol.nl>
2021-02-22 15:14:30 +0100

    first commit

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+s3cmd put --acl-public afspraak.html   s3://www.hairstudiopinella.nl
+s3cmd put --acl-public index.html   s3://www.hairstudiopinella.nl
+s3cmd put --acl-public robots.txt   s3://www.hairstudiopinella.nl
